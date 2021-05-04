48eadd835ef2b58f36147329789bd20c13ecc288
PatrickPicca <patrickpicca@gmail.com>
2021-05-04 09:22:15 -0700

    All tests included in test.cpp

--- test.cpp
+++ test.cpp
@@ -3,11 +3,9 @@
 
 #include "testing.hpp"
 #include "select.hpp"
-#include <iostream>
+#include "Select_Contains_test.hpp"
 #include "Select_Not_test.hpp"
-//#include "Select_And_test.hpp"
 #include "Select_Or_test.hpp"
-
 #include "Select_And_test.hpp"
 
 int main(int argc, char **argv) {
