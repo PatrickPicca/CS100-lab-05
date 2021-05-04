62eee03e1c29480abcc26b81319bc66032d84e3b
Amneh Alsuqi <aalsu013@ucr.edu>
2021-05-04 00:04:23 -0700

    print selection with test (3 files total)

--- spreadsheet.cpp
+++ spreadsheet.cpp
@@ -15,6 +15,26 @@ void Spreadsheet::set_selection(Select* new_select)
     select = new_select;
 }
 
+
+	//criteria for selecting rows is chosen by the user by setting the select pointer
+	//to point to an object that implements that criteria
+	//if select pointer is null, all rows should be printed 
+	
+
+	//if (select == NULL){
+	//print all rows
+	//nested for loop, outer loop: columns, inner loop: rows
+
+		
+//	else {
+//		for(int i = 0; i < data.size(); i++ {
+//			for(int j = 0; i < data.at(i).size(); j++){
+	
+
+
+
+
+
 void Spreadsheet::clear()
 {
     column_names.clear();
@@ -40,3 +60,39 @@ int Spreadsheet::get_column_by_name(const std::string& name) const
             return i;
     return -1;
 }
+
+void Spreadsheet::print_selection(std::ostream& out) const {
+
+        //criteria for selecting rows is chosen by the user by setting the select pointer
+                //to point to an object that implements that criteria
+                        //if select pointer is null, all rows should be printed 
+        
+        
+         if (select == NULL){ 
+             //print all rows
+             //nested for loop, outer loop: columns, inner loop: rows
+         	for(int i = 0; i < data.size(); i++) {  
+                	for (int j = 0; j < data.at(i).size();j++) {
+                        	out << data[i].at(j) << " ";
+                        }
+                   	 out << std::endl;
+                }
+	}
+	else {
+		for(int i = 0; i < data.size(); i++) {                    
+                        for (int j = 0; j < data.at(i).size();j++) {
+                               if(select->select(this, i)){
+					out << data[i].at(j) << " ";
+				}
+			}
+			out << std:: endl;
+
+		}
+
+	}
+}
+
+
+
+
+
