1854a624365b29ea0dde8056c5a98ad0780ebc49
PatrickPicca <patrickpicca@gmail.com>
2021-05-04 08:56:50 -0700

    Finished tests for And & Not

--- test.cpp
+++ test.cpp
@@ -4,7 +4,8 @@
 #include "testing.hpp"
 #include "select.hpp"
 #include <iostream>
-
+#include "Select_Not_test.hpp"
+#include "Select_And_test.hpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
